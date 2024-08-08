5389e3b06e509c6695e9b270cfecb6bc5d0288e4
Taylor Li <258266864@qq.com>
2024-08-08 22:57:17 +0800

    AT-4 headers and data response promise

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import { AxiosRequestConfig } from './types'
-import xhr from './xhr'
+import { transformRequest } from './helpers/data.ts'
+import { processHeaders } from './helpers/headers.ts'
+import { buildURL } from './helpers/url'
+import { AxiosPromise, AxiosRequestConfig } from './types'
+import xhr from './xhr.ts'
 
-function axios(config: AxiosRequestConfig) {
-  xhr(config).json()
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config)
 }
 
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
 export default axios
